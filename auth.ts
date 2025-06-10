e5c96fd38787932216ab16ed60160b565fc62406
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2025-06-10 20:59:22 -0300

    First Commit

---
+++ auth.ts
@@ -0,0 +1,36 @@
+import type { NextAuthConfig } from "next-auth";
+import NextAuth from "next-auth";
+
+import Google from "next-auth/providers/google";
+
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const config = {
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  providers: [Google],
+  callbacks: {
+    session({ session, token }) {
+      if (token.sub) session.user.userId = token.sub;
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/signIn",
+  },
+} satisfies NextAuthConfig;
+
+export const { handlers, auth, signIn, signOut } = NextAuth(config);
+
+interface ProviderWithId {
+  id: string;
+  name: string;
+}
+
+export const providerMap = config.providers.map((provider) => {
+  const typedProvider = provider as unknown as ProviderWithId;
+  return { id: typedProvider.id, name: typedProvider.name };
+});
---
+++ src/app/signin/page.tsx
@@ -0,0 +1,38 @@
+import { signIn, providerMap } from "auth";
+
+import { BsGoogle } from "react-icons/bs";
+
+const icons = [{ name: "Google", icon: <BsGoogle /> }];
+
+const SingnInPage = async () => {
+  const findIcon = (name: string) => {
+    const icon = icons.find((item) => item.name === name);
+    return icon?.icon ?? "";
+  };
+
+  return (
+    <div className="w-1/2 mx-auto my-10 px-4 flex flex-col">
+      <h2 className="text-[2rem] leading-10 font-semibold text-center">
+        Acesse ou crie sua conta com uma das opções disponíveis
+      </h2>
+      {Object.values(providerMap).map((provider) => (
+        <form
+          key={provider.id}
+          action={async () => {
+            "use server";
+            await signIn(provider.id, { redirectTo: "/" });
+          }}
+          className="mt-10 flex justify-center">
+          <button className="cursor-pointer h-10 px-6 py-1 font-medium border border-zinc-600 flex items-center gap-2 rounded hover:bg-slate-50">
+            {findIcon(provider.name)}
+            <span>
+              Entrar com o <strong>{provider.name}</strong>
+            </span>
+          </button>
+        </form>
+      ))}
+    </div>
+  );
+};
+
+export default SingnInPage;
