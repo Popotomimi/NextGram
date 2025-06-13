182a917f3eca59b0d4da3cf75a3ea287ef164520
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2025-06-12 21:23:42 -0300

    Add Actions and update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,37 +1,62 @@
+import { getUserByEmail } from "@/actions";
 import { auth, signOut } from "auth";
 import Link from "next/link";
+import Image from "next/image";
+import Button from "./Button";
+import ButtonLink from "./ButtonLink";
 
 const Navbar = async () => {
   const session = await auth();
 
+  const user = await getUserByEmail(session?.user.email);
+
   return (
-    <nav className="bg-gray-800 text-white p-4 flex justify-between items-center">
-      <Link href="/" className="text-white text-lg font-bold">
-        Home
+    <nav className="bg-gray-800 text-white px-10 py-5 flex justify-between items-center">
+      <Link
+        href="/"
+        className="text-white hover:text-zinc-200 text-lg font-bold">
+        NextGram
       </Link>
       <div>
-        {session && session.user ? (
+        {user ? (
           // logado
           <div className="flex gap-4 items-center">
-            {/* login social => name, email, image */}
-            <p>{session.user.name}</p>
+            <p className="text-white font-medium">{user.name}</p>
+            {user.image && (
+              <Image
+                src={user.image}
+                alt={`Perfil de: ${user.name}`}
+                className="w-10 h-10 rounded-full"
+                width={40}
+                height={40}
+              />
+            )}
+            <Link
+              href={"/profile"}
+              className="text-white font-medium hover:text-zinc-200">
+              Perfil
+            </Link>
+            <Link
+              href={"/post/new"}
+              className="text-white font-medium hover:text-zinc-200">
+              Criar Postagem
+            </Link>
+            <Link
+              href={"/my-posts"}
+              className="text-white font-medium hover:text-zinc-200">
+              Minhas Postagens
+            </Link>
             <form
               action={async () => {
                 "use server";
                 await signOut();
               }}>
-              <button className="bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded">
-                Sair
-              </button>
+              <Button text="Sair" danger={true} type="submit" />
             </form>
           </div>
         ) : (
           //deslogado
-          <Link
-            href="/signin"
-            className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded">
-            Entrar
-          </Link>
+          <ButtonLink text="Entrar" url="/signin" />
         )}
       </div>
     </nav>
